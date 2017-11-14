10a8adf629117356f6625cb3b760514951d00903
Enyiwork <32691424+Enyiwork@users.noreply.github.com>
2017-11-14 09:48:12 -0500

    almost done need css now

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,33 @@
 // Iteration #1
+require("../config/mongoose-setup");
+
+const DroneModel = require("../models/drone");
+
+
+const droneInfo = [
+  {
+    droneName: "D1000",
+    propellers: 6,
+    maxSpeed: 18
+  },
+  {
+    droneName: "Mr. Marcos",
+    propellers: 4,
+    maxSpeed: 14
+  },
+  {
+    droneName: "Mr. Robert",
+    propellers: 2,
+    maxSpeed: 20
+  }
+];
+
+
+DroneModel.create(droneInfo)
+  .then((droneResult) => {
+    console.log("inserted ${droneResult.length} drones");
+  })
+  .catch((err) => {
+    console.log("Drone insert ERROR!");
+    console.log(err);
+  });
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,53 @@
 const express = require('express');
 
 // require the Drone model here
+const DroneModel = require("../models/drone");
 
 const router = express.Router();
 
-
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  DroneModel
+  .find()
+  .limit(25)
+  .sort({ dateAdded: -1})
+  .exec()
+  .then((droneResult) => {
+    res.locals.listOfDrones = droneResult;
+    res.render("drones/drone-list");
+
+  })
+  .catch((err) => {
+
+    next(err);
+
+  });
 });
 
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render("drones/drone-form");
 });
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
-});
+  const theDrone = new DroneModel({
+  droneName:       req.body.droneName,
+  propellers: req.body.dronePropellers,
+  maxSpeed:   req.body.droneSpeed,
+
+  });
+  theDrone.save()
+  .then(() => {
+    res.redirect("/drones");
+  })
+  .catch((err) => {
+    console.log("Drone ERROR");
+    console.log(err);
+   });
+  });
+
+
 
 module.exports = router;
